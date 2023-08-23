5a7e9c79a8fd697eb08b7cfcdf54cb10cf2269fd
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-08-23 23:27:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Breeds from './components/breeds/Breeds';
+
+function App() {
+  return (
+    <div className="App">
+      <Breeds />
+    </div>
+  );
+}
+
+export default App;
