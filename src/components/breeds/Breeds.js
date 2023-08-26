c1b577ec49981aceb77916e01e070a1fe2266235
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-08-26 22:29:23 +0100

    merged:merged breeds conflict

--- src/components/breeds/Breeds.js
+++ src/components/breeds/Breeds.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import './breeds.css'
 
@@ -7,14 +7,3 @@ const Breeds = () => {
         <div></div>
     )
 }
-=======
-// import React from 'react'
-
-// const Breeds = () => {
-//     return (
-//         <div>Breeds</div>
-//     )
-// }
->>>>>>> e05e1b37087c5b69fe7d907dec1a6a45adf39ac2
-
-// export default Breeds
